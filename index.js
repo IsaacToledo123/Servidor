256096d36ef260d6a719c40ab48898331a07eea0
IsaacToledo123 <223245@ids.upchiapas.edu.mx>
2024-07-30 09:47:58 -0600

    push

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const http = require('http');
+const https = require('https'); // Change from http to https
+const fs = require('fs');
 const socketIo = require('socket.io');
 require('dotenv').config();
 
@@ -11,16 +12,21 @@ const PORT = process.env.PORT || 3000;
 const connectToDatabase = require('./connection/connect');
 connectToDatabase();
 
+// SSL Certificate
+const options = {
+  key: fs.readFileSync('/etc/nginx/ssl/nginx-selfsigned.key'), // Path to your private key
+  cert: fs.readFileSync('/etc/nginx/ssl/nginx-selfsigned.crt') // Path to your certificate
+};
 
+// Routes
 const notificaciones = require('./routes/routesNotificaciones');
 const usuario = require('./routes/routesClientes');
 const respuesta = require('./routes/routesNotiRes');
-const rutina=require('./routes/rutinasR');
-const semana =require("./routes/semanaR");
-const ejercicio=require('./routes/ejerciciosR');
-const pago=require('./routes/pagosR');
+const rutina = require('./routes/rutinasR');
+const semana = require('./routes/semanaR');
+const ejercicio = require('./routes/ejerciciosR');
+const pago = require('./routes/pagosR');
 
-const { log } = require('console');
 app.use('/api', notificaciones);
 app.use('/api', usuario);
 app.use('/api', respuesta);
@@ -29,14 +35,8 @@ app.use('/api', semana);
 app.use('/api', ejercicio);
 app.use('/api', pago);
 
-
-
-
-
-const server = app.listen(PORT, () => {
-  console.log(`Servidor en ejecución en el puerto ${PORT}`);
-});
-
+// Create HTTPS server
+const server = https.createServer(options, app);
 
 const io = socketIo(server, {
   cors: {
@@ -48,22 +48,26 @@ const io = socketIo(server, {
 let rooms = 0;
 
 io.on('connection', (socket) => {
-    console.log('Nuevo cliente conectado');
+  console.log('Nuevo cliente conectado');
 
-    const roomName = `Room-${++rooms}`;
-    socket.join(roomName);
-    io.emit('newRoom', roomName);
+  const roomName = `Room-${++rooms}`;
+  socket.join(roomName);
+  io.emit('newRoom', roomName);
 
-    socket.on('joinRoom', room => {
-      socket.join(room)
-    })
-  
-    socket.on('clientMessage', (message) => {
-        io.to(roomName).emit('adminMessage', message);
-        console.log(roomName,message);
-    });
-    socket.on('disconnect', () => {
-      console.log('Cliente desconectado');
+  socket.on('joinRoom', room => {
+    socket.join(room);
   });
-    
+
+  socket.on('clientMessage', (message) => {
+    io.to(roomName).emit('adminMessage', message);
+    console.log(roomName, message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Servidor en ejecución en el puerto ${PORT}`);
 });
